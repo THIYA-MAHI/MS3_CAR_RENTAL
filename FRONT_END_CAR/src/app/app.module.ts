8a452efd7266b7c3c974e591abea0ad5c27cdb49
Thiviya <ut01208tic2024@gmail.com>
2024-11-05 01:06:57 +0900

    about page

--- FRONT_END_CAR/src/app/app.module.ts
+++ FRONT_END_CAR/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './LANDING_PAGE/components/landing-page/landing-page.component';
+import { ContactUsComponent } from './LANDING_PAGE/components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
